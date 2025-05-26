0a04cd382b941ca949bdcb2234b6e912f621a069
Nawriz0 <nawrizalawatdinov@icloud.com>
2025-05-27 03:18:57 +0500

    Добавил нужные файлы и строки связанные с gunicorn

--- QuickShop/settings.py
+++ QuickShop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['onreder.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'QuickShop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
